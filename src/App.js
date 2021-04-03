85ea22e1826321fac4586bb14f334db3c1f0066e
Ezran Bayantemur <ezranbayantemur@gmail.com>
2021-04-03 14:57:47 +0300

    feat: :ambulance: Flexbox layout props

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ function App() {
   // https://reactnative.dev/docs/flexbox
   return (
     <SafeAreaView style={{flex: 1}}>
-      <View style={{flex: 3, backgroundColor: 'red'}}>
-        <Text>VIEW 1</Text>
-      </View>
-      <View style={{flex: 1, backgroundColor: 'blue'}}>
-        <Text>VIEW 2</Text>
-      </View>
-      <View style={{flex: 2, backgroundColor: 'green'}}>
-        <Text>VIEW 3</Text>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'aqua',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          // flexDirection: 'row',
+        }}>
+        <View style={{width: 100, height: 100, backgroundColor: 'red'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'blue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'green'}} />
       </View>
     </SafeAreaView>
   );
