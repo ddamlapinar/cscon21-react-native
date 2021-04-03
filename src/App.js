5a2745bf0f1c756b4ccd07140ff787ae4f1f2f57
Ezran Bayantemur <ezranbayantemur@gmail.com>
2021-04-03 13:30:52 +0300

    Component basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Button, Text, Alert} from 'react-native';
 
 function App() {
+  // VARIABLES
+  const number = 5;
+  const result = number * 2;
+  const user = {id: 1, name: 'Ezran', age: 27};
+
+  // LOGGING
+  console.log('Selam! ', result);
+
+  // REGULAR (NAMED) FUNCTION
+  // function handlePress() {
+  //   const displayName = user.name.toUpperCase();
+  //   Alert.alert('Merhaba ' + displayName);
+  // }
+
+  // ARROW FUNCTION
+  const handlePress = () => {
+    const displayName = user.name.toUpperCase();
+    Alert.alert('Merhaba ' + displayName);
+  };
+
   return (
     <SafeAreaView>
-      <Text>Hello World</Text>
+      {/* COMPONENT PROP */}
+      <Text style={{fontSize: 40, fontWeight: 'bold'}}>Selamlar!!</Text>
+      <Text>Sonuç: {result + 1}</Text>
+      <Button title="Gönder" onPress={handlePress} />
     </SafeAreaView>
   );
 }
