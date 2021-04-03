d2ab595cadc5368544e969e61eadbb6db86b693c
Ezran Bayantemur <ezranbayantemur@gmail.com>
2021-04-03 17:13:54 +0300

    fix: :rocket: Upgrading data list with FlatList component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, FlatList} from 'react-native';
 import UserCard from './components/UserCard/UserCard';
 
 const userList = [
   {
-    id: 0,
+    user_id: 0,
     name: 'Ezran Bayantemur',
     tag: 'barbatus',
     content:
       'Ut deserunt proident ad occaecat ut Lorem. Culpa dolor labore tempor dolor aliqua mollit sunt do amet. Laborum irure esse labore',
   },
   {
-    id: 1,
+    user_id: 1,
     name: 'Mert Erim',
     tag: 'Mochte',
     content:
       'Mollit consequat ex aliqua est excepteur eiusmod cupidatat officia incididunt pariatur ad consequat incididunt. Proident non officia aliqua dolore consectetur ipsum labore. Eiusmod est dolore labore incididunt consectetur exercitation do minim ex nostrud tempor.',
   },
   {
-    id: 2,
+    user_id: 2,
+    name: 'Didem Gülşen Damlapınar',
+    tag: 'ddamlapinar',
+    content:
+      'Ea dolore mollit et duis in culpa cupidatat cupidatat consectetur ad dolore amet cillum. Eiusmod ullamco labore est commodo occaecat minim voluptate anim do fugiat laborum exercitation ea fugiat. Sit aliqua duis deserunt excepteur ipsum ea officia. Veniam excepteur esse ex nisi nisi.',
+  },
+  {
+    user_id: 3,
+    name: 'Didem Gülşen Damlapınar',
+    tag: 'ddamlapinar',
+    content:
+      'Ea dolore mollit et duis in culpa cupidatat cupidatat consectetur ad dolore amet cillum. Eiusmod ullamco labore est commodo occaecat minim voluptate anim do fugiat laborum exercitation ea fugiat. Sit aliqua duis deserunt excepteur ipsum ea officia. Veniam excepteur esse ex nisi nisi.',
+  },
+  {
+    user_id: 4,
     name: 'Didem Gülşen Damlapınar',
     tag: 'ddamlapinar',
     content:
@@ -34,19 +48,20 @@ function App() {
 
   return (
     <SafeAreaView style={{flex: 1}}>
-      <Text style={{fontSize: 40, fontWeight: 'bold'}}>USERS</Text>
-
-      {userList.map(user => {
-        return (
-          <UserCard
-            key={user.id}
-            username={user.name}
-            usertag={user.tag}
-            content={user.content}
-            onSelect={() => handleSelectUser(user.tag)}
-          />
-        );
-      })}
+      <FlatList
+        keyExtractor={item => item.user_id.toString()}
+        data={userList}
+        renderItem={({item}) => {
+          return (
+            <UserCard
+              username={item.name}
+              usertag={item.tag}
+              content={item.content}
+              onSelect={() => handleSelectUser(item.tag)}
+            />
+          );
+        }}
+      />
     </SafeAreaView>
   );
 }
