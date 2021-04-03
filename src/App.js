200efcb0d196e810df5e2ddb1fa7858ca941253e
Ezran Bayantemur <ezranbayantemur@gmail.com>
2021-04-03 16:58:10 +0300

    feat: Listing with map

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import UserCard from './components/UserCard/UserCard';
 
+const userList = [
+  {
+    id: 0,
+    name: 'Ezran Bayantemur',
+    tag: 'barbatus',
+    content:
+      'Ut deserunt proident ad occaecat ut Lorem. Culpa dolor labore tempor dolor aliqua mollit sunt do amet. Laborum irure esse labore',
+  },
+  {
+    id: 1,
+    name: 'Mert Erim',
+    tag: 'Mochte',
+    content:
+      'Mollit consequat ex aliqua est excepteur eiusmod cupidatat officia incididunt pariatur ad consequat incididunt. Proident non officia aliqua dolore consectetur ipsum labore. Eiusmod est dolore labore incididunt consectetur exercitation do minim ex nostrud tempor.',
+  },
+  {
+    id: 2,
+    name: 'Didem Gülşen Damlapınar',
+    tag: 'ddamlapinar',
+    content:
+      'Ea dolore mollit et duis in culpa cupidatat cupidatat consectetur ad dolore amet cillum. Eiusmod ullamco labore est commodo occaecat minim voluptate anim do fugiat laborum exercitation ea fugiat. Sit aliqua duis deserunt excepteur ipsum ea officia. Veniam excepteur esse ex nisi nisi.',
+  },
+];
+
 function App() {
   function handleSelectUser(username) {
     console.log('SELECTED USER: ' + username);
@@ -10,26 +34,19 @@ function App() {
 
   return (
     <SafeAreaView style={{flex: 1}}>
-      <Text>USERS</Text>
-      <UserCard
-        username="Ezran Bayantemur"
-        usertag="barbatus"
-        content="Ut deserunt proident ad occaecat ut Lorem. Culpa dolor labore tempor
-        dolor aliqua mollit sunt do amet. Laborum irure esse labore"
-        onSelect={() => handleSelectUser('Ezran')}
-      />
-      <UserCard
-        username="Mert Erim"
-        usertag="Mochte"
-        content="Mollit consequat ex aliqua est excepteur eiusmod cupidatat officia incididunt pariatur ad consequat incididunt. Proident non officia aliqua dolore consectetur ipsum labore. Eiusmod est dolore labore incididunt consectetur exercitation do minim ex nostrud tempor."
-        onSelect={() => handleSelectUser('Mert')}
-      />
-      <UserCard
-        username="Didem Gülşen Damlapınar"
-        usertag="ddamlapinar"
-        content="Ea dolore mollit et duis in culpa cupidatat cupidatat consectetur ad dolore amet cillum. Eiusmod ullamco labore est commodo occaecat minim voluptate anim do fugiat laborum exercitation ea fugiat. Sit aliqua duis deserunt excepteur ipsum ea officia. Veniam excepteur esse ex nisi nisi."
-        onSelect={() => handleSelectUser('Didem')}
-      />
+      <Text style={{fontSize: 40, fontWeight: 'bold'}}>USERS</Text>
+
+      {userList.map(user => {
+        return (
+          <UserCard
+            key={user.id}
+            username={user.name}
+            usertag={user.tag}
+            content={user.content}
+            onSelect={() => handleSelectUser(user.tag)}
+          />
+        );
+      })}
     </SafeAreaView>
   );
 }
